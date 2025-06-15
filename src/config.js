f8ffeab9b6532d0d9df70b7f8eb535d69b28be2b
Констанца <bashconstanza@proton.me>
2025-06-15 07:16:44 +0200

    Create config.js

---
+++ src/config.js
@@ -0,0 +1,10 @@
+export const EURO_ADDRESS = "0x6B49eD944b97D44Ae0dfEEE0fc92E0E5927DB4B3";
+export const KYBERSWAP_ROUTER = "0x9AAb3f75489902f3a48495025729a0AF77d4b11e"; // BSC router
+
+export const CHAINS = {
+  BSC: {
+    chainId: 56,
+    name: "BNB Smart Chain",
+    rpcUrl: "https://bsc-dataseed.binance.org/"
+  }
+};
